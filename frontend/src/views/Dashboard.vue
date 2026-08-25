18a10a019df22978cb0154bec541445cf955e799
SatoInu <87768647+SatoDoge@users.noreply.github.com>
2026-08-26 06:33:03 +0900

    オンラインメンバー表示ウィジェットを追加

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,9 +1,11 @@
 <script setup>
+import OnlineMemberWidget from '@/components/dashboard/OnlineMemberWidget.vue';
 import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
         <StatsWidget />
+        <OnlineMemberWidget />
     </div>
 </template>
