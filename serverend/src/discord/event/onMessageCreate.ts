2e10d205b7d80e91297ed34b20d0bd5d92a05254
SatoInu <87768647+SatoDoge@users.noreply.github.com>
2026-08-27 13:11:19 +0900

    メッセージ受信イベントを実装

---
+++ serverend/src/discord/event/onMessageCreate.ts
@@ -0,0 +1,27 @@
+import { getDiscordClient } from '#server/discord.js';
+import { Events, type Message } from 'discord.js';
+import { Logger } from '#server/utils/logger.js';
+
+const logger = new Logger('discord.event.onMessageCreate');
+
+export function onMessageCreate() {
+    const client = getDiscordClient();
+    if (!client) {
+        logger.error('Discord client not found');
+        return;
+    }
+    client.on(Events.MessageCreate, (message) => {
+        void handleMessageCreate(message);
+    });
+}
+
+export async function handleMessageCreate(message: Message) {
+    // DM の場合は無視
+    if (!message.inGuild()) {
+        return;
+    }
+
+    // Botのメッセージは無視
+    if (message.author.bot) return;
+    
+}
